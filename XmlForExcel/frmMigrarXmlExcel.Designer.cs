0ea1b1b7da51cc0ad2f818b5a8592e6bafdca954
Silva <Fabricio.Silva@bnymellon.com>
2020-02-21 12:05:20 -0300

    primeiro commit

---
+++ XmlForExcel/frmMigrarXmlExcel.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace XmlForExcel
+{
+  partial class frmMigrarXmlExcel
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.btnBrowseFolder = new System.Windows.Forms.Button();
+      this.txtXmlFilePath = new System.Windows.Forms.TextBox();
+      this.btnConvert = new System.Windows.Forms.Button();
+      this.OFD = new System.Windows.Forms.OpenFileDialog();
+      this.progressBar1 = new System.Windows.Forms.ProgressBar();
+      this.lblQtdArquivos = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(14, 12);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(118, 13);
+      this.label1.TabIndex = 27;
+      this.label1.Text = "Selecione Arquivo XML";
+      // 
+      // btnBrowseFolder
+      // 
+      this.btnBrowseFolder.Location = new System.Drawing.Point(236, 27);
+      this.btnBrowseFolder.Name = "btnBrowseFolder";
+      this.btnBrowseFolder.Size = new System.Drawing.Size(29, 23);
+      this.btnBrowseFolder.TabIndex = 26;
+      this.btnBrowseFolder.Text = "...";
+      this.btnBrowseFolder.UseVisualStyleBackColor = true;
+      this.btnBrowseFolder.Click += new System.EventHandler(this.btnBrowseFolder_Click);
+      // 
+      // txtXmlFilePath
+      // 
+      this.txtXmlFilePath.Location = new System.Drawing.Point(14, 29);
+      this.txtXmlFilePath.Name = "txtXmlFilePath";
+      this.txtXmlFilePath.Size = new System.Drawing.Size(218, 20);
+      this.txtXmlFilePath.TabIndex = 25;
+      // 
+      // btnConvert
+      // 
+      this.btnConvert.Location = new System.Drawing.Point(14, 55);
+      this.btnConvert.Name = "btnConvert";
+      this.btnConvert.Size = new System.Drawing.Size(63, 23);
+      this.btnConvert.TabIndex = 24;
+      this.btnConvert.Text = "Converter";
+      this.btnConvert.UseVisualStyleBackColor = true;
+      this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+      // 
+      // OFD
+      // 
+      this.OFD.Filter = "XML File (*.xml)|*.xml|All files (*.*)|*.*";
+      this.OFD.Multiselect = true;
+      this.OFD.FileOk += new System.ComponentModel.CancelEventHandler(this.OFD_FileOk);
+      // 
+      // progressBar1
+      // 
+      this.progressBar1.Location = new System.Drawing.Point(1, 117);
+      this.progressBar1.Name = "progressBar1";
+      this.progressBar1.Size = new System.Drawing.Size(315, 23);
+      this.progressBar1.TabIndex = 30;
+      // 
+      // lblQtdArquivos
+      // 
+      this.lblQtdArquivos.AutoSize = true;
+      this.lblQtdArquivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblQtdArquivos.ForeColor = System.Drawing.Color.Red;
+      this.lblQtdArquivos.Location = new System.Drawing.Point(4, 97);
+      this.lblQtdArquivos.Name = "lblQtdArquivos";
+      this.lblQtdArquivos.Size = new System.Drawing.Size(52, 17);
+      this.lblQtdArquivos.TabIndex = 31;
+      this.lblQtdArquivos.Text = "           ";
+      // 
+      // frmMigrarXmlExcel
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(315, 140);
+      this.Controls.Add(this.lblQtdArquivos);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.btnBrowseFolder);
+      this.Controls.Add(this.txtXmlFilePath);
+      this.Controls.Add(this.btnConvert);
+      this.Controls.Add(this.progressBar1);
+      this.Name = "frmMigrarXmlExcel";
+      this.Text = "XML --->> EXCEL";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowseFolder;
+        private System.Windows.Forms.TextBox txtXmlFilePath;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.OpenFileDialog OFD;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblQtdArquivos;
+    }
+}
---
+++ XmlForExcel/frmMigrarXmlExcel.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Data;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+
+namespace XmlForExcel
+{
+  public partial class frmMigrarXmlExcel : Form
+  {
+    public frmMigrarXmlExcel()
+    {
+      InitializeComponent();
+    }
+
+    private void btnBrowseFolder_Click(object sender, EventArgs e)
+    {
+
+
+      DialogResult drResult = OFD.ShowDialog();
+      if (drResult == DialogResult.OK)
+        txtXmlFilePath.Text = OFD.FileName;
+    }
+
+    private void btnConvert_Click(object sender, EventArgs e)
+    {
+      try
+      {
+
+
+        if (txtXmlFilePath.Text != "") // sem usar nome padrao
+        {
+          // start progressbar
+
+          var Total = OFD.FileNames.Count();
+          int i = 0;
+          foreach (String file in OFD.FileNames)
+          {
+            
+            progressBar1.Value = 0;
+
+            //if (chkCustomeName.Checked && txtCustomeFileName.Text != "" && txtXmlFilePath.Text != "") // Verifica se usa o nome selecionado
+            //{
+            //  if (File.Exists(txtXmlFilePath.Text)) // verifica se existe o arquivo xml
+            //  {
+            //    string CustXmlFilePath = Path.Combine(new FileInfo(txtXmlFilePath.Text).DirectoryName, txtCustomeFileName.Text); // verifica pasta para o xml
+            //    System.Data.DataTable dt = CreateDataTableFromXml(txtXmlFilePath.Text);
+            //    ExportDataTableToExcel(dt, CustXmlFilePath);
+
+            //  }
+
+            //} 
+            //else
+
+            if (File.Exists(file)) // verifica se existe o arquivo xml
+            {
+              FileInfo fi = new FileInfo(file);
+              string XlFile = fi.DirectoryName + "\\" + fi.Name.Replace(fi.Extension, ".xlsx");
+              System.Data.DataTable dt = CreateDataTableFromXml(file);
+              ExportDataTableToExcel(dt, XlFile);
+
+
+            }
+
+            i += 1;
+            lblQtdArquivos.Text = i+"/" + Total + " arquivo(s) convertido(s).";
+
+          }
+        }
+        else
+        {
+          MessageBox.Show("Preencha os campos obrigatórios!!");
+          return;
+        }
+
+        MessageBox.Show("Conversão completa!");
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show("ocorreu um erro!" + ex.Message);
+        throw;
+      }
+    }
+
+
+    // Criando DataTable Com dados do xml
+    public System.Data.DataTable CreateDataTableFromXml(string XmlFile)
+    {
+
+      System.Data.DataTable Dt = new System.Data.DataTable();
+      try
+      {
+        DataSet ds = new DataSet();
+        ds.ReadXml(XmlFile);
+        Dt.Load(ds.CreateDataReader());
+
+      }
+      catch (Exception ex)
+      {
+
+      }
+      return Dt;
+    }
+
+    private void ExportDataTableToExcel(System.Data.DataTable table, string Xlfile)
+    {
+
+      Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+      Workbook book = excel.Application.Workbooks.Add(Type.Missing);
+      excel.Visible = false;
+      excel.DisplayAlerts = false;
+      Worksheet excelWorkSheet = (Worksheet)book.ActiveSheet;
+      excelWorkSheet.Name = table.TableName;
+
+      progressBar1.Maximum = table.Columns.Count;
+      for (int i = 1; i < table.Columns.Count + 1; i++) // Criando as colunas no excel
+      {
+        excelWorkSheet.Cells[1, i] = table.Columns[i - 1].ColumnName;
+        if (progressBar1.Value < progressBar1.Maximum)
+        {
+          progressBar1.Value++;
+          int percent = (int)(((double)progressBar1.Value / (double)progressBar1.Maximum) * 100);
+          progressBar1.CreateGraphics().DrawString(percent.ToString() + "%", new System.Drawing.Font("Arial", (float)8.25, FontStyle.Regular), Brushes.Black, new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+          System.Windows.Forms.Application.DoEvents();
+        }
+      }
+
+
+      progressBar1.Maximum = table.Rows.Count;
+      for (int j = 0; j < table.Rows.Count; j++) // Exportando as linhas para o excel
+      {
+        for (int k = 0; k < table.Columns.Count; k++)
+        {
+          excelWorkSheet.Cells[j + 2, k + 1] = table.Rows[j].ItemArray[k].ToString();
+        }
+
+        if (progressBar1.Value < progressBar1.Maximum)
+        {
+          progressBar1.Value++;
+          int percent = (int)(((double)progressBar1.Value / (double)progressBar1.Maximum) * 100);
+          progressBar1.CreateGraphics().DrawString(percent.ToString() + "%", new System.Drawing.Font("Arial", (float)8.25, FontStyle.Regular), Brushes.Black, new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+          System.Windows.Forms.Application.DoEvents();
+        }
+      }
+
+
+      book.SaveAs(Xlfile);
+      book.Close(true);
+      excel.Quit();
+
+      Marshal.ReleaseComObject(book);
+      Marshal.ReleaseComObject(book);
+      Marshal.ReleaseComObject(excel);
+
+    }
+
+    private void OFD_FileOk(object sender, CancelEventArgs e)
+    {
+      lblQtdArquivos.Text = OFD.FileNames.Count() + " arquivo(s) selecionado(s).";
+    }
+  }
+}
